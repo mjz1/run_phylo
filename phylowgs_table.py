bcda6ed920028934501d91f6c09942dab14b6447
Vallijah Subasri <vsubasri@qlogin6.hpf.new>
2018-07-06 12:23:46 -0400

    Allowed for multiple physical ids per logical id

--- phylowgs_table.py
+++ phylowgs_table.py
@@ -8,21 +8,18 @@ import re
 import gzip
 import zipfile
 from functools import reduce
-#import rpy2.robjects as robjects
-#from rpy2.robjects import pandas2ri
+import rpy2.robjects as robjects
+from rpy2.robjects import pandas2ri
 
+def parse_phylotab(tab_path):
+	tab = pd.read_csv(tab_path, header=0, delimiter='\t')
+	
 def convert_rda(outdir, paths):
-#	pandas2ri.activate()
+	pandas2ri.activate()
 	rdas = []
 	for path in paths: 
-		#### TEMP ####
-		temp_path = os.path.join(outdir, os.path.splitext(os.path.basename(path))[0]+".tmp") 
-		command = "Rscript /hpf/largeprojects/adam/projects/lfs/run_phylowgs/rda_to_tab.R --mutect " + path + " --outfile " + temp_path
-		subprocess.call(command, shell=True)
-		df = pd.read_csv(temp_path, header=0, delimiter='\t')
-		##############
-	#	rda = robjects.r.load(path)[0]
-	#	df = robjects.r[rda]
+		rda = robjects.r.load(path)[0]
+		df = robjects.r[rda]
 		rdas.append(df)
 	return(rdas)
 
@@ -63,25 +60,30 @@ def create_table(outdir, mutect2):
 
 	with open(best_tree_path) as f:
 	    phylo_json_dict = json.load(f)
-	samples = phylo_json_dict['dataset_name'].split(',')
+	
+	with open(paths['summ_path']) as f:
+            summ_json_dict = json.load(f)
+	
+	samples = summ_json_dict['params']['samples']
 	phylo_muts = pd.DataFrame(phylo_json_dict['mut_assignments']).transpose()
 	muts = pd.concat([pd.DataFrame(phylo_muts.cnvs.values.tolist(),index= phylo_muts.index), pd.DataFrame(phylo_muts.ssms.values.tolist(), index= phylo_muts.index)], axis=1)
 	muts = pd.melt(muts.transpose()).dropna()
 	phylo_dict = dict(zip(muts["value"], muts["variable"]))
 
-
 	cnv_physical = pd.read_csv(paths['cnv_physical_path'], header=0, delimiter='\t')
-	cnv_physical[['chrom','start','end', 'major_cn','minor_cn','cell_prev']] = cnv_physical['physical_cnvs'].str.split(',',expand=True)
-	cnv_physical['gene'] = cnv_physical['physical_cnvs'].str.replace('chrom=|start=|end=|major_cn=|minor_cn=|cell_prev=','').str.split(',', expand=True).loc[: ,0:2].apply(lambda x:'%s_%s_%s' % (x[0],x[1],x[2]),axis=1)
-	cnv_physical[[s + "_a" for s in samples]] = cnv_physical['a'].str.split(',', expand=True)
-	cnv_physical[[s + "_d" for s in samples]] = cnv_physical['d'].str.split(',', expand=True)
+	physical_id = pd.concat([cnv_physical['physical_cnvs'].str.split(';',expand=True),cnv_physical[['cnv','a','d']]],axis=1)
+	physical_id = pd.melt(physical_id, id_vars=['cnv','a', 'd']).dropna()
+	physical_id[['chrom','start','end', 'major_cn','minor_cn','cell_prev']] = physical_id['value'].str.split(',',expand=True)
+	physical_id['value'] = physical_id['value'].str.replace('chrom=|start=|end=|major_cn=|minor_cn=|cell_prev=','').str.split(',', expand=True).loc[: ,0:2].apply(lambda x:'%s_%s_%s' % (x[0],x[1],x[2]),axis=1)
+	physical_id[[s + "_a" for s in samples]] = physical_id['a'].str.split(',', expand=True)
+	physical_id[[s + "_d" for s in samples]] = physical_id['d'].str.split(',', expand=True)
 	ssm_physical = pd.read_csv(paths['ssm_physical_path'], header=0, delimiter='\t')
 	ssm_physical[[s + "_a" for s in samples]] = ssm_physical['a'].str.split(',', expand=True)
 	ssm_physical[[s + "_d" for s in samples]] = ssm_physical['d'].str.split(',', expand=True)	
+	
 	#ssm_dict = ssm_physical[['id'] + [s + "_a" for s in samples]].groupby('id').apply(lambda dfg: dfg.to_dict(orient='list')).to_dict()
-	variants = pd.concat([ssm_physical[['id', 'gene']], cnv_physical[['cnv', 'gene']].rename(columns={'cnv':'id'})], ignore_index=True)
+	variants = pd.concat([ssm_physical[['id', 'gene']], physical_id[['cnv', 'value']].rename(columns={'cnv':'id', 'value':'gene'})], ignore_index=True)
 	variants['clone'] = variants['id'].map(phylo_dict)
-
 	annotated_ssms = convert_rda(outdir, mutect2)
 
 	for annotated_ssm in annotated_ssms:
@@ -90,11 +92,10 @@ def create_table(outdir, mutect2):
 		variants['gene'] = variants['gene'].replace(annotated_ssm_dict)
 
 	## a = ref_reads ; d = total_reads
-	variants_a = pd.concat([ssm_physical[['id'] + [s + "_a" for s in samples]], cnv_physical[['cnv'] + [s + "_a" for s in samples]].rename(columns={'cnv':'id'})], ignore_index=True)
-	variants_d = pd.concat([ssm_physical[['id'] + [s + "_d" for s in samples]], cnv_physical[['cnv'] + [s + "_d" for s in samples]].rename(columns={'cnv':'id'})], ignore_index=True)
+	variants_a = pd.concat([ssm_physical[['id'] + [s + "_a" for s in samples]], physical_id[['cnv'] + [s + "_a" for s in samples]].rename(columns={'cnv':'id'})], ignore_index=True)
+	variants_d = pd.concat([ssm_physical[['id'] + [s + "_d" for s in samples]], physical_id[['cnv'] + [s + "_d" for s in samples]].rename(columns={'cnv':'id'})], ignore_index=True)
 	dfs = [variants, variants_a, variants_d]
 	variants = reduce(lambda left,right: pd.merge(left,right,on='id'), dfs)
-
 	variants.rename(columns={'gene':'snvid'},inplace=True)
 	
 	for path, annotated_ssm in zip(mutect2, annotated_ssms):
@@ -106,7 +107,7 @@ def create_table(outdir, mutect2):
 	cols = annovar + ["ensembl_gene","hgnc_gene","cosmic_census","aa", "snvid"] 
 	ssms_annotated = pd.concat(annotated_ssms)[cols].drop_duplicates(subset=['snvid'], keep='first')
 
-	variants_multisample = pd.merge(variants, ssms_annotated, how='left', on='snvid')
+	variants_multisample = pd.merge(variants, ssms_annotated, how='left', on='snvid').drop_duplicates()
 	outfile = os.path.join(outdir, "results", samples[0].split('_')[0] + "_phylowgs.txt")
 	variants_multisample.to_csv(outfile, index=False, sep='\t')
 #	print(variants.groupby(['clone']).agg({'position':['count',lambda x: ','.join(x)],'id':[lambda y: y.str.contains('c').count(), lambda x: x.str.contains('s').count()]}))
@@ -118,12 +119,11 @@ def main():
 		formatter_class=argparse.ArgumentDefaultsHelpFormatter
 	)
 
-	parser.add_argument('-o','--outdir', dest='outdir', default=os.getcwd(),
-		help='Output destination for variants')
+	
+	parser.add_argument('-o','--outdir', required=True , dest='outdir', help='Output destination for variants')
 	parser.add_argument('-m','--mutect', nargs='+', dest='mutect2', help='Annotated rda for EACH sample in multisample', required=True)
 
 	args = parser.parse_args()
-
 	create_table(args.outdir, args.mutect2)
 	
 
